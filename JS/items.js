cb6cfa35bd4298835f510e428fe34380be4e26d8
Jun Dolor <jun.dolor@gmail.com>
2019-10-10 00:31:44 +0800

    item submit to order summary

--- JS/items.js
+++ JS/items.js
@@ -6,8 +6,11 @@ const subitems = [];
 //const radio_img =  document.querySelectorAll('.radio-image');
 const item_details = document.querySelector('#items-details');
 const product_size = document.querySelector('#product-size');
+const product_weight = document.querySelector('#product-weight');
+const item_price = document.querySelector('#item-price');
 
 const cards = document.querySelector('#cards');
+const btnNext = document.querySelector('#next');
 
 (function(){
 	fetch(url)
@@ -101,6 +104,7 @@ const nextStep = (e) => {
 	console.log(e.target.dataset.sizeList);
 	console.log(e.target.dataset.priceList);
 
+	let item_prod = e.target.value;
 	let size_arr = e.target.dataset.sizeList.split(':');
 	let price_arr = e.target.dataset.priceList.split(':');
 
@@ -118,8 +122,83 @@ const nextStep = (e) => {
 			item_details.className = 'flex-form form-full';
 		}
 
+		document.querySelector('#selected h3').textContent = `Choose your ${item_prod} size and weight`;
+
+		compute_price();
+
 		document.querySelector('#selected').scrollIntoView({ 
 		  behavior: 'smooth' 
 		});
 	}
 }
+
+const compute_price = () => {
+	let selected_size_arr = product_size.value.split(':');
+	let size = selected_size_arr[0];
+	let prize = parseFloat(selected_size_arr[1]);
+	let selected_weight = parseFloat(product_weight.value);
+	let subitem_prod = document.querySelector(`input[name="${item}"]:checked`).value;
+
+	let total_prize = prize * selected_weight;
+
+
+	item_price.textContent = `Total for ${subitem_prod}, ${selected_weight} kg: ${total_prize}`;
+}
+
+product_size.addEventListener('change', compute_price);
+product_weight.addEventListener('change', compute_price);
+
+const reset = () => {
+	let subitem_arr = document.querySelectorAll(`input[name="${item}"]`);
+
+	subitem_arr.forEach(st => {
+		st.checked = false;
+	});
+
+	document.querySelector('#selected h3').textContent = "";
+
+	product_size.innerHTML = '';
+
+	product_weight.selectedIndex = "0";
+
+	item_price.textContent = '';
+
+	item_details.className = 'flex-form form-init';
+}
+
+const order_summary_palengke = () => {
+	let subitem_prod = document.querySelector(`input[name="${item}"]:checked`).value;
+
+	let selected_size_arr = product_size.value.split(':');
+	let size = selected_size_arr[0];
+	let prize = parseFloat(selected_size_arr[1]);
+
+	let prod_details = `${size} Size / Php ${prize}`;
+
+	let total_prize = prize * parseFloat(product_weight.value);
+
+	let ordersObj = Object.create(null);
+	ordersObj.style = style;
+	ordersObj.item = json_key;
+	ordersObj.subitem = subitem_prod;
+	ordersObj.prod_details = prod_details;
+	ordersObj.weight = product_weight.value;
+	ordersObj.market_price = total_prize;
+	ordersObj.restaurant = '';
+	ordersObj.paluto = '';
+	ordersObj.paluto_price = '';
+
+	let str = JSON.stringify(ordersObj);
+
+	let orders_url = encodeURI(str);
+
+	reset();
+
+	window.location.href = `order_summary.html?orders=${orders_url}`;
+}
+
+btnNext.addEventListener('click', e => {
+	e.preventDefault();
+
+	order_summary_palengke();
+});
